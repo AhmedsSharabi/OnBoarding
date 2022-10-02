c881fbc02e945c88fee2e85f477ec03b2bad9c4a
Ahmed Sharabi <ahmeds.alsharabi@gmail.com>
2022-10-02 13:43:46 +0200

    Initial Commit

---
+++ OnboardingTest/OnboardingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingTestApp.swift
+//  OnboardingTest
+//
+//  Created by Ahmed Sharabi on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
